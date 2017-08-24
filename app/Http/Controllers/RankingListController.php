bb516d597e166c035c35b73f3a6dd4bf190069c6
中国天妖 <656861622@qq.com>
2017-08-24 15:13:22 +0800

    mmm

--- app/Http/Controllers/RankingListController.php
+++ app/Http/Controllers/RankingListController.php
@@ -72,7 +72,7 @@ class RankingListController extends Controller
 
         foreach ($data as $value) {
 
-            $value['NAME'] = substr_replace($value['NAME'], '**', 0, strlen($value['NAME'])-3);
+            $value['NAME'] = substr_replace($value['NAME'], '**', 3, strlen($value['NAME']));
 
             $value['LOGIN'] = substr_replace($value['LOGIN'], '****', 0, strlen($value['LOGIN'])-2);
 
